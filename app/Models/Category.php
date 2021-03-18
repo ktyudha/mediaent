74d2aca9388ed5318d2f8af50469ffb86db11f19
khoiron14 <khoironarkade@gmail.com>
2021-03-18 17:13:12 +0700

    refactor category article

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -11,6 +11,8 @@ class Category extends Model
 
     protected $fillable = ['name'];
 
+    public $timestamps = false;
+
     public function articles()
     {
         return $this->hasMany(Article::class);
--- database/migrations/2021_03_04_115545_create_articles_table.php
+++ database/migrations/2021_03_04_115545_create_articles_table.php
@@ -16,6 +16,7 @@ class CreateArticlesTable extends Migration
         Schema::create('articles', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained();
+            $table->foreignId('category_id')->constrained();
             $table->string('title');
             $table->string('slug');
             $table->text('body');
