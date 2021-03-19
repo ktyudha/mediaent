ba554279b7b54709b7d4339cdd2852d62419c30f
khoiron14 <khoironarkade@gmail.com>
2021-03-19 12:46:18 +0700

    refactor controller

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -2,13 +2,9 @@
 
 namespace App\Http\Controllers\Admin;
 
-use App\Models\User;
-use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 class HomeController extends Controller
 {
-    public function index() {
-        return view('admin.home');
-    }
+    
 }
